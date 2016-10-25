898662947f5e92198868412750bf231fc369eb2b
Emily Main <emilymuirmain@gmail.com>
2016-10-25 11:11:58 -0700

    adds index view

--- app/controllers/garments_controller.rb
+++ app/controllers/garments_controller.rb
@@ -1,13 +1,17 @@
 class GarmentsController < ApplicationController
   def index
+    @garments =Garment.all
   end
 
   def show
+      @garments = Garment.find(params[:id])
+  end
+
+  def new
   end
 
   def edit
   end
 
-  def new
-  end
+
 end
